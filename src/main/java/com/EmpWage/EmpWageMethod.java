4ecf6dc2a9c6eda2478dc293e25c17c7aa554d13
shwetap29 <shwetaprasad48@gmail.com>
2021-05-24 20:28:07 +0530

    REFACTORED: Switch case

--- src/main/java/com/EmpWage/EmpWageMethod.java
+++ src/main/java/com/EmpWage/EmpWageMethod.java
@@ -1,17 +1,19 @@
 package com.EmpWage;
 
 public class EmpWageMethod {
+
+    public static final int IS_FULL_TIME=1;
+    public static final int IS_PART_TIME=2;
+    public static final int WAGE_PER_HR=20;
+
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
-        int IS_PART_TIME=2;
+        int IS_PART_TIME = 2;
         int WAGE_PER_HR = 20;
+
         int empHr = 0;
         int dailyWage = 0;
-
-        System.out.println("Welcome to the Employee Wage Computation");
-
-        int checkEmp = (int) (Math.random()*2);
-        int checkEmp = (int) (Math.random()*3);
+        int checkEmp = (int) (Math.random() * 3);
 
         if (checkEmp == IS_FULL_TIME) {
             System.out.println("Employee is Present");
@@ -21,7 +23,7 @@ public class EmpWageMethod {
             empHr = 0;
 
             empHr=8;
-        }  elseif (checkEmp==IS_PART_TIME) {
+        }  else if (checkEmp==IS_PART_TIME); {
             empHr=4;
         }
         dailyWage = empHr * WAGE_PER_HR;
