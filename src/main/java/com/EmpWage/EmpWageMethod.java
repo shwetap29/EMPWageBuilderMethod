4844b41f3924aef91c9aa39d4ab5eb7ebe5ee514
shwetap29 <shwetaprasad48@gmail.com>
2021-05-24 18:46:20 +0530

    ADDED: Welcome Message

---
+++ src/main/java/com/EmpWage/EmpWageMethod.java
@@ -0,0 +1,8 @@
+package com.EmpWage;
+
+public class EmpWageMethod {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee Wage Computation");
+    }
+}
+
