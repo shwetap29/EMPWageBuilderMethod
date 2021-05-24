76e187558537d6b0a412ead27b2900d27de101ca
shwetap29 <shwetaprasad48@gmail.com>
2021-05-24 20:49:46 +0530

    CALCULATED: Wage For month

--- src/main/java/com/EmpWage/EmpWageMethod.java
+++ src/main/java/com/EmpWage/EmpWageMethod.java
@@ -1,39 +1,45 @@
 package com.EmpWage;
 
 public class EmpWageMethod {
-
     public static final int IS_FULL_TIME=1;
     public static final int IS_PART_TIME=2;
     public static final int WAGE_PER_HR=20;
+    public static final int MAX_WORKING_DAYS=20;
 
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int WAGE_PER_HR = 20;
 
-        int empHr = 0;
-        int dailyWage = 0;
-        int checkEmp = (int) (Math.random() * 3);
+        int empHr=0;
+        int dailyWage=0;
+        int totalWage=0;
 
-        if (checkEmp == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-            empHr = 8;
-        } else {
-            System.out.println("Employee is Absent");
-            empHr = 0;
 
-            empHr=8;
-        }  else if (checkEmp==IS_PART_TIME); {
-            empHr=4;
+        System.out.println("Wellcome to the Employee Wage computation program.");
+
+
+        for (int i =1; i<=MAX_WORKING_DAYS; i++) {
+
+            int checkEmp = (int) (Math.random()*3);
+
+            switch (checkEmp) {
+                case IS_FULL_TIME :
+                    empHr=8;
+                    break;
+                case IS_PART_TIME :
+                    empHr=4;
+                    break;
+                default :
+                    empHr=0;
+            }
+
+            dailyWage=empHr*WAGE_PER_HR;
+            totalWage += dailyWage;
+
         }
-        dailyWage = empHr * WAGE_PER_HR;
-           else {
-            empHr=0;
-        }
-        dailyWage=empHr*WAGE_PER_HR;
-        System.out.println("Daily wage = " + dailyWage);
-
+        System.out.println("Total wage for month = "+totalWage);
     }
 
 }
+
+
+
 
