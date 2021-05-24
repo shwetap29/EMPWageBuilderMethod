7a34f76feed285affa0daf4e5406b1011257c1eb
shwetap29 <shwetaprasad48@gmail.com>
2021-05-24 19:58:06 +0530

    ADDED:Part Time Wage

--- src/main/java/com/EmpWage/EmpWageMethod.java
+++ src/main/java/com/EmpWage/EmpWageMethod.java
@@ -3,21 +3,32 @@ package com.EmpWage;
 public class EmpWageMethod {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME=2;
         int WAGE_PER_HR = 20;
-        int EmpHr = 0;
+        int empHr = 0;
         int dailyWage = 0;
 
         System.out.println("Welcome to the Employee Wage Computation");
 
-        int checkEmp = 0;
+        int checkEmp = (int) (Math.random()*2);
+        int checkEmp = (int) (Math.random()*3);
+
         if (checkEmp == IS_FULL_TIME) {
             System.out.println("Employee is Present");
-            EmpHr = 8;
+            empHr = 8;
         } else {
             System.out.println("Employee is Absent");
-            EmpHr = 0;
+            empHr = 0;
+
+            empHr=8;
+        }  elseif (checkEmp==IS_PART_TIME) {
+            empHr=4;
         }
-        dailyWage = EmpHr * WAGE_PER_HR;
+        dailyWage = empHr * WAGE_PER_HR;
+           else {
+            empHr=0;
+        }
+        dailyWage=empHr*WAGE_PER_HR;
         System.out.println("Daily wage = " + dailyWage);
 
     }
