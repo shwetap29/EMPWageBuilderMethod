1845193d60f4cbf6da99afcc50162a8417c32842
shwetap29 <shwetaprasad48@gmail.com>
2021-05-24 19:08:25 +0530

    ADDED: Emp check

--- src/main/java/com/EmpWage/EmpWageMethod.java
+++ src/main/java/com/EmpWage/EmpWageMethod.java
@@ -2,7 +2,19 @@ package com.EmpWage;
 
 public class EmpWageMethod {
     public static void main(String[] args) {
+        int IS_FULL_TIME=1;
         System.out.println("Welcome to the Employee Wage Computation");
+        int checkEmp = (int) (Math.random()*2);
+
+        if (checkEmp==IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+
     }
+
 }
+
 
