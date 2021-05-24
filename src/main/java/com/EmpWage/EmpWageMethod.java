fbce667da1fc9336889ab6534d9fc7e8a7c82cb0
shwetap29 <shwetaprasad48@gmail.com>
2021-05-24 21:37:57 +0530

    REFACTORED:code by oops concept

--- src/main/java/com/EmpWage/EmpWageMethod.java
+++ src/main/java/com/EmpWage/EmpWageMethod.java
@@ -7,36 +7,33 @@ public class EmpWageMethod {
     public static final int MAX_WORKING_DAYS=20;
     public static final int MAX_HR_IN_MONTH=100;
 
+
     public static void main(String[] args) {
         int totalWage=0;
         int totalWorkingDays=0;
         int totalHr=0;
 
-
         System.out.println("Welcome to the Employee Wage computation program.");
 
-
-
         while (totalWorkingDays<MAX_WORKING_DAYS && totalHr<MAX_HR_IN_MONTH) {
-            int empHr=0;
-            totalWorkingDays++;
+                int empHr = 0;
+                totalWorkingDays++;
+                int checkEmp = (int) (Math.random() * 3);
 
-            int checkEmp = (int) (Math.random()*3);
+                switch (checkEmp) {
+                    case IS_FULL_TIME:
+                        empHr = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHr = 4;
+                        break;
+                    default:
+                        empHr = 0;
+                }
+                totalHr += empHr;
 
-            switch (checkEmp) {
-                case IS_FULL_TIME :
-                    empHr=8;
-                    break;
-                case IS_PART_TIME :
-                    empHr=4;
-                    break;
-                default :
-                    empHr=0;
-            }
-            totalHr += empHr;
-
-            int dailyWage=empHr*WAGE_PER_HR;
-            System.out.println("Daily Wage ="+dailyWage);
+                int dailyWage = empHr * WAGE_PER_HR;
+                System.out.println("Daily Wage =" + dailyWage);
 
         }
         totalWage=totalHr*WAGE_PER_HR;
