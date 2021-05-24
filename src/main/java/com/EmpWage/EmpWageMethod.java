79edd181ef410fea685d3e5daffa3994091a898a
shwetap29 <shwetaprasad48@gmail.com>
2021-05-24 19:44:12 +0530

    REFACTORED: calculated daily wage

--- src/main/java/com/EmpWage/EmpWageMethod.java
+++ src/main/java/com/EmpWage/EmpWageMethod.java
@@ -2,19 +2,25 @@ package com.EmpWage;
 
 public class EmpWageMethod {
     public static void main(String[] args) {
-        int IS_FULL_TIME=1;
-        System.out.println("Welcome to the Employee Wage Computation");
-        int checkEmp = (int) (Math.random()*2);
+        int IS_FULL_TIME = 1;
+        int WAGE_PER_HR = 20;
+        int EmpHr = 0;
+        int dailyWage = 0;
 
-        if (checkEmp==IS_FULL_TIME) {
+        System.out.println("Welcome to the Employee Wage Computation");
+
+        int checkEmp = 0;
+        if (checkEmp == IS_FULL_TIME) {
             System.out.println("Employee is Present");
+            EmpHr = 8;
         } else {
             System.out.println("Employee is Absent");
+            EmpHr = 0;
         }
-
+        dailyWage = EmpHr * WAGE_PER_HR;
+        System.out.println("Daily wage = " + dailyWage);
 
     }
 
 }
-
 
